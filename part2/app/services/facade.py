8a7a4110910498fe3f15c93ea612ce2327656ce7
HB10065 <10065@holbertonstudents.com>
2025-06-19 17:30:56 +0000

    copy/paste

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
@@ -17,3 +16,27 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+    def create_review(self, review_data):
+        # Placeholder for logic to create a review, including validation for user_id, place_id, and rating
+        pass
+
+    def get_review(self, review_id):
+        if type(review_id) is not str:
+            raise TypeError('id must be a string')
+
+    def get_all_reviews(self):
+        # Placeholder for logic to retrieve all reviews
+        pass
+
+    def get_reviews_by_place(self, place_id):
+        if type(place_id) is not str:
+            raise TypeError('id must be a string')
+
+    def update_review(self, review_id, review_data):
+        if type(review_id) is not str:
+            raise TypeError('id must be a string')
+
+    def delete_review(self, review_id):
+        if type(review_id) is not str:
+            raise TypeError('id must be a string')
