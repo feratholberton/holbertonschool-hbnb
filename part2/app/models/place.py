bdef29754b887d4a1e2983f68d861c02e6e0aa8b
Fer <fernando.falcon@holbertonstudents.com>
2025-06-19 14:17:07 -0300

    Add amenities relation and improve error messages

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -6,27 +6,26 @@ class Place(BaseModel):
     def __init__(self, title, price, latitude, longitude, owner, description=""):
         super().__init__()
         if not title or len(title) > 100:
-            raise ValueError("too many characters!")
+            raise ValueError("The title of the place. Required, maximum length of 100 characters.")
         
         if price < 0:
-                raise ValueError("cannot be negative")
+                raise ValueError("Must be a positive value.")
         
         if not (-90 <= latitude <= 90):
-            raise ValueError("incorrect latitude")
+            raise ValueError("Latitude coordinate for the place location. Must be within the range of -90.0 to 90.0.")
         
         if not(-180 <= longitude <= 180):
-             raise ValueError("incorrect length")
+             raise ValueError("Longitude coordinate for the place location. Must be within the range of -180.0 to 180.0.")
         
         if not isinstance(owner, User):
             raise ValueError("Owner must be a User")
-        
-        #self.id = id
+
         self.title = title
         self.description = description
         self.price = price
         self.latitude = latitude
         self.longitude = longitude
         self.owner = owner
-       
         self.reviews = []
+        self.amenities = []
         owner.places.append(self)
