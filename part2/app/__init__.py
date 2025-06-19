c8908b115ef7d6ef9782134b656b3ba5679e3402
HB10065 <10065@holbertonstudents.com>
2025-06-19 19:05:25 +0000

    added namespace for reviews

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.reviews import api as reviews_namesapce
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
-    # Placeholder for API namespaces (endpoints will be added later)
+    api.add_namespace(reviews_namesapce, path='/reviews')
     # Additional namespaces for places, reviews, and amenities will be added later
 
     return app
