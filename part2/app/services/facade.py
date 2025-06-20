181dce909156d64a0ab6209b54b1ec25099184e3
HB10065 <10065@holbertonstudents.com>
2025-06-20 23:18:50 +0000

    Review methods in facade(placeholders)

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -4,7 +4,9 @@ from app.models.user import User
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-
+    
+    # User Methods -----------------------------------------------------------
+    
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -15,7 +17,9 @@ class HBnBFacade:
 
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
-
+    
+    # Amenities Methods -------------------------------------------------------
+    
     def create_amenity(self, amenity_data):
     # Placeholder for logic to create an amenity
         pass
@@ -30,4 +34,30 @@ class HBnBFacade:
 
     def update_amenity(self, amenity_id, amenity_data):
     # Placeholder for logic to update an amenity
+        pass
+    
+    # Reviews Methods ----------------------------------------------------------
+    
+    def create_review(self, review_data):
+        # Placeholder for logic to create a review, including validation for user_id, place_id, and rating
+        pass
+
+    def get_review(self, review_id):
+        # Placeholder for logic to retrieve a review by ID
+        pass
+
+    def get_all_reviews(self):
+        # Placeholder for logic to retrieve all reviews
+        pass
+
+    def get_reviews_by_place(self, place_id):
+        # Placeholder for logic to retrieve all reviews for a specific place
+        pass
+
+    def update_review(self, review_id, review_data):
+        # Placeholder for logic to update a review
+        pass
+
+    def delete_review(self, review_id):
+        # Placeholder for logic to delete a review
         pass
