27aa97133673d3877d76e957557334deea33fd43
gastii11 <10632@holbertonstudents.com>
2025-06-20 17:22:29 -0300

    2. User Endpoints

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,42 +1,14 @@
-from app.persistence.repository import InMemoryRepository
-
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
 
-    def create_review(self, review_data):
-        # Placeholder for logic to create a review, including validation for user_id, place_id, and rating
-        pass
-
-    def get_review(self, review_id):
-        if type(review_id) is not str:
-            raise TypeError('id must be a string')
-
-    def get_all_reviews(self):
-        # Placeholder for logic to retrieve all reviews
-        pass
-
-    def get_reviews_by_place(self, place_id):
-        if type(place_id) is not str:
-            raise TypeError('id must be a string')
-
-    def update_review(self, review_id, review_data):
-        if type(review_id) is not str:
-            raise TypeError('id must be a string')
-
-    def delete_review(self, review_id):
-        if type(review_id) is not str:
-            raise TypeError('id must be a string')
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
