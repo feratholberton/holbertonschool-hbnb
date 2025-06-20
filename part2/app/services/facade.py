b724a92c16b88eb3cc48ab4477de528f3d629000
gastii11 <10632@holbertonstudents.com>
2025-06-20 17:29:44 -0300

    Amenity Endpoints

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -12,3 +12,19 @@ class HBnBFacade:
 
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
+
+    def create_amenity(self, amenity_data):
+    # Placeholder for logic to create an amenity
+    pass
+
+    def get_amenity(self, amenity_id):
+    # Placeholder for logic to retrieve an amenity by ID
+        pass
+
+    def get_all_amenities(self):
+    # Placeholder for logic to retrieve all amenities
+        pass
+
+    def update_amenity(self, amenity_id, amenity_data):
+    # Placeholder for logic to update an amenity
+        pass
