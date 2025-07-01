23220392df8416e2b748d3d4cfd6e57ee1af86ab
gastii11 <10632@holbertonstudents.com>
2025-07-01 15:43:44 -0300

    parte 3, ejercicio 0

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -4,9 +4,15 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from config import DevelopmentConfig
+from flask_bcrypt import Bcrypt
 
-def create_app():
+bcrypt = Bcrypt()
+
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
+    app.config.from_object(config_class)
+
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     # Register the users namespace
@@ -15,5 +21,3 @@ def create_app():
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     return app
-
-
