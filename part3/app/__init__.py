45805d72b0d435ba363287e30b55883ee9093478
Fer <fer.falcon.dg@gmail.com>
2025-07-08 14:50:58 -0300

    2. Implement JWT Authentication with `flask-jwt-extended`

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -4,14 +4,17 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns
 from config import DevelopmentConfig
 from app.extensions import bcrypt
+from app.extensions import jwt
 
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
@@ -20,4 +23,5 @@ def create_app(config_class=DevelopmentConfig):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     return app
